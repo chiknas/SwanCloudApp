79fcab4dd782d71932e0e93ed89e5df235bba8c9
chiknas <nkuknas@gmail.com>
2020-12-30 23:24:42 +0000

    add background task support

--- App.tsx
+++ App.tsx
@@ -1,15 +1,66 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StatusBar, useColorScheme} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigation from 'navigation/Navigation';
 import useCachedResources from 'hooks/useCachedResources';
 import {HeaderStyles} from 'constants/Header';
+import BackgroundFetch from 'react-native-background-fetch';
+
+// Background task for androids that runs when the app is terminated
+BackgroundFetch.registerHeadlessTask(async (event) => {
+  console.log('[BackgroundFetch HeadlessTask] start: ', event.taskId);
+  BackgroundFetch.finish(event.taskId);
+});
 
 //TODO: for navigation to work on iOS we might need cocoapods https://reactnative.dev/docs/navigation
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    // Configure it.
+    BackgroundFetch.configure(
+      {
+        minimumFetchInterval: 15, // <-- minutes (15 is minimum allowed)
+        // Android options
+        forceAlarmManager: false, // <-- Set true to bypass JobScheduler.
+        stopOnTerminate: false,
+        enableHeadless: true,
+        startOnBoot: true,
+        requiredNetworkType: BackgroundFetch.NETWORK_TYPE_NONE, // Default
+        requiresCharging: false, // Default
+        requiresDeviceIdle: false, // Default
+        requiresBatteryNotLow: false, // Default
+        requiresStorageNotLow: false, // Default
+      },
+      async (taskId) => {
+        console.log('[js] Received background-fetch event: ', taskId);
+        // Required: Signal completion of your task to native code
+        // If you fail to do this, the OS can terminate your app
+        // or assign battery-blame for consuming too much background-time
+        BackgroundFetch.finish(taskId);
+      },
+      (error) => {
+        console.log('[js] RNBackgroundFetch failed to start');
+      },
+    );
+
+    // Optional: Query the authorization status.
+    BackgroundFetch.status((status) => {
+      switch (status) {
+        case BackgroundFetch.STATUS_RESTRICTED:
+          console.log('BackgroundFetch restricted');
+          break;
+        case BackgroundFetch.STATUS_DENIED:
+          console.log('BackgroundFetch denied');
+          break;
+        case BackgroundFetch.STATUS_AVAILABLE:
+          console.log('BackgroundFetch is enabled');
+          break;
+      }
+    });
+  }, [BackgroundFetch]);
 
   if (!isLoadingComplete) {
     return null;
